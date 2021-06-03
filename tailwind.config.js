724ec9799169a2c50aeb8bb90c8208d112737c7f
mohammad mehrabani <mehrabani.developer@gmail.com>
2021-06-04 01:12:55 +0430

    import custome font

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      body: ['iranyekan','َArial'],
+      display: ['iranyekan','Arial']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
